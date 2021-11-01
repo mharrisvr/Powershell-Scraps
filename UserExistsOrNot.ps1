c5f8f98cd5334dcbb42e9fa02996857ed7bd4b3b
Mike <mharrisvr@users.noreply.github.com>
2021-11-01 16:06:19 -0400

    Error handing
    Added exception handling for if user ID doesn't exist

--- UserExistsOrNot.ps1
+++ UserExistsOrNot.ps1
@@ -5,9 +5,19 @@ Import-Module ActiveDirectory
 
 $employeeIDs = @('1','2','3','4','5','6','7','8','9','10')
 
+Write-Host "`n....Checking EmployeeIDs`n" -ForegroundColor Green
+
 $Users = foreach ($employeeID in $employeeIDs) {
-    $User = Get-ADUser -filter ("EmployeeID -eq $employeeID")
-    Get-ADUser $User.DistinguishedName -properties *
-}
+    $User = $(try {Get-ADUser -filter ("EmployeeID -eq $employeeID") } catch {$null})
+    if ($User -ne $Null) {
+        Get-ADUser $User.DistinguishedName -properties *
+        }
+    else {
+        Write-Host "User with $employeeID does not exist"
+        }
 
 $Users | select name, employeeID, description
+
+# This part just used for testing to verify # Of users that were checked VS # that actually existed
+$employeeIDs.count
+$Users.count 
